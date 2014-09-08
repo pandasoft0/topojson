b49490b03985033a14dfe6f8f37423048d0c9ee3
pandasoft0 <pandasoft0@gmail.com>
2014-09-07 17:25:57 -0700

    Regenerate world examples.

--- topojson.js
+++ topojson.js
@@ -1,6 +1,6 @@
 !function() {
   var topojson = {
-    version: "1.6.17",
+    version: "1.6.18",
     mesh: function(topology) { return object(topology, meshArcs.apply(this, arguments)); },
     meshArcs: meshArcs,
     merge: function(topology) { return object(topology, mergeArcs.apply(this, arguments)); },
